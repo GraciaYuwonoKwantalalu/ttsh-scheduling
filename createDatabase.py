d25ac6440fdd6de9a472bf4493c8e4060f0341a2
ccneo97 <54536291+Shigomashi@users.noreply.github.com>
2021-03-01 01:40:10 +0800

    Creating a database folder with empty sqlite database

---
+++ createDatabase.py
@@ -0,0 +1,144 @@
+import sqlite3
+from sqlFunctionCalls import create_connection, close_connection
+
+conn, cur = create_connection()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS Constraints(
+   constraint_id INTEGER PRIMARY KEY AUTOINCREMENT,
+   doctor_call_daily INTEGER NOT NULL,
+   day_off_monthly INTEGER NOT NULL,
+   max_call_month_four INTEGER NOT NULL,
+   max_call_month_five INTEGER NOT NULL
+   );
+""")
+conn.commit()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS Roster(
+   email TEXT PRIMARY KEY,
+   name TEXT NOT NULL,
+   first_position TEXT,
+   second_position TEXT,
+   posting TEXT,
+   points INTEGER
+   );
+""")
+conn.commit()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS Skill(
+   email TEXT,
+   skill TEXT,
+   PRIMARY KEY (email, skill),
+   FOREIGN KEY (email) REFERENCES Roster (email)
+   );
+""")
+conn.commit()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS Training(
+   training_id INTEGER PRIMARY KEY AUTOINCREMENT,
+   email TEXT NOT NULL,
+   name TEXT NOT NULL,
+   training TEXT NOT NULL,
+   start_date TEXT NOT NULL,
+   end_date TEXT NOT NULL,
+   FOREIGN KEY (email) REFERENCES Roster (email)
+   );
+""")
+conn.commit()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS Duty(
+   duty_id INTEGER PRIMARY KEY AUTOINCREMENT,
+   email TEXT NOT NULL,
+   name TEXT NOT NULL,
+   duty_name TEXT NOT NULL,
+   start_date TEXT NOT NULL,
+   end_date TEXT NOT NULL,
+   FOREIGN KEY (email) REFERENCES Roster (email)
+   );
+""")
+conn.commit()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS PriorityLeave(
+   pl_id INTEGER PRIMARY KEY AUTOINCREMENT,
+   email TEXT NOT NULL,
+   name TEXT NOT NULL,
+   reason TEXT NOT NULL,
+   start_date TEXT NOT NULL,
+   end_date TEXT NOT NULL,
+   FOREIGN KEY (email) REFERENCES Roster (email)
+   );
+""")
+conn.commit()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS LeaveApplication(
+   leave_id INTEGER PRIMARY KEY AUTOINCREMENT,
+   email TEXT NOT NULL,
+   name TEXT NOT NULL,
+   start_date TEXT NOT NULL,
+   end_date TEXT NOT NULL,
+   duration TEXT NOT NULL,
+   leave_type TEXT NOT NULL,
+   remark TEXT,
+   FOREIGN KEY (email) REFERENCES Roster (email)
+   );
+""")
+conn.commit()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS LeaveLP(
+   leave_id INTEGER PRIMARY KEY AUTOINCREMENT,
+   email TEXT NOT NULL,
+   name TEXT NOT NULL,
+   start_date TEXT NOT NULL,
+   end_date TEXT NOT NULL,
+   duration TEXT NOT NULL,
+   leave_type TEXT NOT NULL,
+   remark TEXT,
+   FOREIGN KEY (email) REFERENCES Roster (email)
+   );
+""")
+conn.commit()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS CallRequest(
+   call_id INTEGER PRIMARY KEY AUTOINCREMENT,
+   email TEXT NOT NULL,
+   name TEXT NOT NULL,
+   date TEXT NOT NULL,
+   request_type TEXT NOT NULL,
+   remark TEXT,
+   FOREIGN KEY (email) REFERENCES Roster (email)
+   );
+""")
+conn.commit()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS CallLP(
+   call_id INTEGER PRIMARY KEY AUTOINCREMENT,
+   email TEXT NOT NULL,
+   name TEXT NOT NULL,
+   date TEXT NOT NULL,
+   request_type TEXT NOT NULL,
+   remark TEXT,
+   FOREIGN KEY (email) REFERENCES Roster (email)
+   );
+""")
+conn.commit()
+
+close_connection(conn, cur)
+
+
+#Format Reference: Create table in sqlite
+'''
+cur.execute("""CREATE TABLE IF NOT EXISTS users(
+   userid INT PRIMARY KEY,
+   fname TEXT,
+   lname TEXT,
+   gender TEXT);
+""")
+conn.commit()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS orders(
+   orderid INT PRIMARY KEY,
+   date TEXT,
+   userid TEXT,
+   total TEXT);
+""")
+conn.commit()
+'''
