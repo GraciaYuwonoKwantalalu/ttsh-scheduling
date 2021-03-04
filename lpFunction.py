f74b0adc4a0603c43b54bb9efa6e7e0322d89851
ccneo97 <54536291+Shigomashi@users.noreply.github.com>
2021-03-04 15:40:51 +0800

    updated this file to prepare for actual LP codes

--- lpFunction.py
+++ lpFunction.py
@@ -1,4 +1,5 @@
 #import PuLP
 
-def run_lp(doctor_call_daily,day_off_monthly,max_call_month_4,max_call_month_5, duty_results, training_results, pl_results):
+def run_lp(doctor_call_daily,day_off_monthly,max_call_month_4,max_call_month_5, duty_results, training_results, pl_results, ph_results):
     return ""
+
