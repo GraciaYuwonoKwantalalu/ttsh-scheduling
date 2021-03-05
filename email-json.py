45edc6a196c69ca21ff808b636a032c991b70709
Weichen Gao <73623835+wcgao8888@users.noreply.github.com>
2021-03-05 13:50:47 +0800

    Updated email-json.py
    1) shift the date filtering within the for loop
    2) pull the json, filter the json to question & answer, append it to the final list output

--- email-json.py
+++ email-json.py
@@ -1,6 +1,7 @@
-import datetime
+from datetime import datetime
 from win32com.client import Dispatch
 from pprint import pprint
+import json
 
 outlook = Dispatch("Outlook.Application").GetNamespace("MAPI")
 root_folder = outlook.Folders.Item(1)
@@ -12,60 +13,85 @@ root_folder = outlook.Folders.Item(1)
 test_folder = root_folder.Folders['test']
 messages=test_folder.Items
 
-email_dict = []
+email_list = []
+email_body_list = []
 count = 0
 
 for message in messages:
-    ### email subjects
-    emailSubject = message.Subject
-    
-    ### email body
-    emailBody = message.Body
-    
-    ### sender name
-    emailSender = message.Sender
-    
-    ### sender email address
-    emailSenderAddress = message.Sender.Address
-    
-    ### Get email date 
-    date = message.SentOn.strftime("%d-%m-%y")
-    
+
     count += 1
+
+    ### Get email date 
+    date = str(message.SentOn)  
+    date = date[:8]    
+    year_of_date = date[6:]
+    month_of_date = date[3:5]
     
-    case = {
-        "ID": count,
-        "Subject": emailSubject,
-        "Body":emailBody,
-        "SenderEmail": emailSenderAddress,
-        "SenderName":emailSender,
-        "Date":date
-    }
+    if year_of_date == '21':
+        if month_of_date == "05":
+
     
-    email_dict.append(case)
+            ### email subjects
+            emailSubject = message.Subject
+            
+            
+            ### email body
+            emailBody = message.Body
+            emailBody = emailBody.encode("utf-8")
 
-#pprint(email_dict)
+            
+            
+            ### sender name
+            emailSender = message.Sender
+            
+            
+            ### sender email address
+            emailSenderAddress = message.Sender.Address
+            
 
-### format the string
-sampleJson = email_dict[3]['Body']
-formatJson = sampleJson.split()
-formatJson = ''.join(formatJson)
+            formatJson = emailBody.split()
+            formatJson = ''.join(formatJson)
+            start = formatJson.find('--StartofJSON--') + 15
+            end = formatJson.find('--EndofJSON--')
+            myJson = formatJson[start:end]
+            #print(myJson)   
+            #print(type(myJson)) # str
+            myQuestion = myJson.split('{"question":')
+            #pprint(myQuestion) # list
+            
+            count2 = 0
+            for i in myQuestion:
+                myAnswerPosition = i.find('"answer"')
+                myQuestion = i[1:myAnswerPosition-2]
+                #print(myQuestion)
+                myAns = i[myAnswerPosition:]
+                myAnswerPosition = myAns.find('},')
+                myAns = myAns[:myAnswerPosition]
+                myAnswerPosition = myAns.find('"answer":"') + 10
+                myAns = myAns[myAnswerPosition:]
+                myAns = myAns[:len(myAns)-1]
+                #print(myAns)
+                
+                count2 +=1
+                
+                case2 = {
+                    "ID": count2,
+                    "question":myQuestion,
+                    "answer":myAns
+                }
+                email_body_list.append(case2)
+            
+            
+            # each case represents an email
+            case = {
+                "ID": count,
+                "Subject": emailSubject,
+                "Body":email_body_list,
+                "SenderEmail": emailSenderAddress,
+                "SenderName":emailSender,
+                "Date":date
+            }
+            
+            email_list.append(case)
 
-### pull JSON
-start = formatJson.find('--StartofJSON--')
-end = formatJson.find('--EndofJSON--')
-pprint(formatJson[start+15:end])
-
-### filter by dates
-#pprint(email_dict)
-for email in email_dict:
-    theDate = ''
-    for i in email['Date']:
-        theDate += i
-    #print(theDate)
-    
-    # check for year 2021
-    if theDate[6:] == '21':
-        # check for month
-        if theDate[3:5] =='01':
-            print('jan')
+pprint(email_list)
