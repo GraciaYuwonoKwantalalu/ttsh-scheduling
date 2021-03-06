259b1b8016cd165e70df3b8c6dedc05e55afd107
Weichen Gao <73623835+wcgao8888@users.noreply.github.com>
2021-03-06 08:11:17 +0800

    created days-for-dates.py
    1) given a range of dates, from a start n end date, we list out the dates and which day is it from

---
+++ days-for-dates.py
@@ -0,0 +1,33 @@
+from datetime import date, timedelta
+import datetime
+from pprint import pprint
+
+# format: (yyyy, m, d) or (yyyy,m,dd) or (yyyy,mm,dd)
+start_date  = datetime.datetime(2021, 3, 1)
+
+end_date  = datetime.datetime(2021, 3, 6)
+
+# as timedelta
+delta = end_date - start_date
+
+datesList = []
+
+### weekdays as a tuple
+weekDays = ("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+
+for i in range(delta.days + 1):
+    day = start_date + timedelta(days=i)
+    dateOnly = day.strftime("%Y-%m-%d")
+    #print(dateOnly)
+    day = day.weekday()
+    day_weekday = weekDays[day]
+    #print(day_weekday)
+    
+    case = {
+            "DATE": dateOnly,
+            "Day": dateOnly,
+        }
+    
+    datesList.append(case)
+    
+pprint(datesList)
