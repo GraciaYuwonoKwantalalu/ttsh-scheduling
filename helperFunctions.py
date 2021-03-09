2f92f4d027319c0e49dc637034b6deacd03b97e4
ccneo97 <54536291+ChuanCheng97@users.noreply.github.com>
2021-03-09 19:22:10 +0800

    changed function file name
    also added in a check weekend function

--- helperFunctions.py
+++ helperFunctions.py
@@ -1,5 +1,6 @@
-import sqlite3
+import sqlite3, datetime
 from sqlite3 import Error
+from datetime import date, timedelta
 
 def create_connection():
     """ create a database connection to a SQLite database """
@@ -17,3 +18,13 @@ def close_connection(conn, cur):
         conn.close()
     except Error as e:
         print(e)
+
+def check_weekend(date):
+    weekend = {5: "Saturday", 6: "Sunday"}
+    
+    # Check what is the value for the date
+    num = date.weekday()    # returns a value from 0-6 where 0 is Monday and 6 is Sunday
+    if num in weekend:
+        return True
+    else:
+        return False
