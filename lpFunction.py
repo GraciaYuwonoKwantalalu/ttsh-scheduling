3f29a89603d6e925dd38d98f3bfca8fee5f9a4f6
ccneo97 <54536291+ChuanCheng97@users.noreply.github.com>
2021-03-13 01:27:56 +0800

    prepping LPfunction file to consider the constraints
    not confirmed changes, subject to changes later depending on LP team

--- lpFunction.py
+++ lpFunction.py
@@ -1,5 +1,7 @@
 #import PuLP
 
-def run_lp(doctor_call_daily,day_off_monthly,max_call_month_4,max_call_month_5, duty_results, training_results, pl_results, ph_results):
+def run_lp(doctor_call_daily, day_off_monthly, max_call_month_4, max_call_month_5, 
+                total_call, clinic1, clinic2, amSat_clinic4, amSat_clinic1, amSat_clinic3, p, 
+                duty_results, training_results, pl_results, ph_results):
     return ""
 
