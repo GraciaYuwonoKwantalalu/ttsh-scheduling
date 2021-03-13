654ce3abfb7b204d907b4a3715fbb4f9b2863bb6
ccneo97 <54536291+ChuanCheng97@users.noreply.github.com>
2021-03-14 01:35:15 +0800

    Updated helperFunction.py to include the constraint_checking function
    1. Edited the constraint checking function to check the Temp table instead of previously checking the LP tables

--- helperFunctions.py
+++ helperFunctions.py
@@ -1,6 +1,6 @@
-import sqlite3, datetime
+import sqlite3, datetime, json
 from sqlite3 import Error
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 
 def create_connection():
     """ create a database connection to a SQLite database """
@@ -29,7 +29,7 @@ def check_weekend(date):
     else:
         return False
 
-def is_constraint_met():
+def is_constraint_met(table_name, start_date, end_date):
     try:
         # Establish connection to DB
         conn, cur = create_connection()
@@ -44,61 +44,86 @@ def is_constraint_met():
         amSat_clinic1 = constraints_results[9]
         amSat_clinic3 = constraints_results[10]
         p = constraints_results[11]
+    
+    except Exception as e:
+        return (str(e))
 
-        # Retrieve from DB the current scheduling call and duty numbers
-        cur.execute("""SELECT COUNT(*) FROM CallLP where request_type = "c" or request_type = "cr";""")
-        constraints_results = cur.fetchone()
-        current_total_call = constraints_results[0]
+    try:
+        # Manipulating the dates for the function to work
+        sdate = datetime.strptime(start_date, '%Y-%m-%d').date()   # start date
+        edate = datetime.strptime(end_date, '%Y-%m-%d').date()   # end date
+        delta = edate - sdate       # as timedelta
 
-        cur.execute("""SELECT COUNT(*) FROM Duty where duty_name = "Clinic 1";""")
-        constraints_results = cur.fetchone()
-        current_clinic1 = constraints_results[0]
+        # Dictionary to store the results that will be returned if constraints are not met in the form: {date:[constraint1,constraint2],date:[constraint1],...}
+        dict_notmet = {}
 
-        cur.execute("""SELECT COUNT(*) FROM Duty where duty_name = "Clinic 2";""")
-        constraints_results = cur.fetchone()
-        current_clinic2 = constraints_results[0]
+        # Creating a loop to check the constraints for each day
+        for date_diff in range(delta.days + 1):
+            day = sdate + timedelta(days=date_diff)     # 2020-08-02 (datetime object format)
+            day_key = day.strftime("%Y-%m-%d")          # 2020-08-02 (string format)
 
-        cur.execute("""SELECT COUNT(*) FROM Duty where duty_name = "amSat Clinic 4";""")
-        constraints_results = cur.fetchone()
-        current_amSat_clinic4 = constraints_results[0]
+            # Retrieve from DB each day's schedule
+            sqlstmt = """SELECT * FROM """ + table_name + """ WHERE date = ?;"""
+            cur.execute(sqlstmt,(day_key,))
+            constraints_result = cur.fetchone()
 
-        cur.execute("""SELECT COUNT(*) FROM Duty where duty_name = "amSat Clinic 1";""")
-        constraints_results = cur.fetchone()
-        current_amSat_clinic1 = constraints_results[0]
+            # Counters to record the number of calls/duties for each day assigned
+            counter_clinic1 = 0
+            counter_clinic2 = 0
+            counter_amsatclinic1 = 0
+            counter_amsatclinic3 = 0
+            counter_amsatclinic4 = 0
+            counter_p = 0
+            counter_totalcall = 0
 
-        cur.execute("""SELECT COUNT(*) FROM Duty where duty_name = "amSat Clinic 3";""")
-        constraints_results = cur.fetchone()
-        current_amSat_clinic3 = constraints_results[0]
+            # Counting the calls/duties from all doctors for each day
+            for element in constraints_result[1:]:
+                str_element = element.replace("'",'"')
+                dict_element = json.loads(str_element)
+                for key,value in dict_element.items():
+                    if value == 'amSat Clinic 1':
+                        counter_amsatclinic1 += 1
+                    elif value == 'Clinic 1':
+                        counter_clinic1 += 1
+                    elif value == 'Clinic 2':
+                        counter_clinic2 += 1
+                    elif value == 'amSat Clinic 3':
+                        counter_amsatclinic2 += 1
+                    elif value == 'amSat Clinic 4':
+                        counter_amsatclinic4 += 1
+                    elif value == 'P':
+                        counter_p += 1
+                    elif value == 'c' or value == 'cr':
+                        counter_totalcall += 1
 
-        cur.execute("""SELECT COUNT(*) FROM Duty where duty_name = "P";""")
-        constraints_results = cur.fetchone()
-        current_p = constraints_results[0]
+            # Compare whether the current schedule meets the constraints
+            not_met = []
+            if counter_totalcall < total_call:
+                not_met.append("total call")
+            if counter_clinic1 < clinic1:
+                not_met.append("clinic 1")
+            if counter_clinic2 < clinic2:
+                not_met.append("clinic 2")
+            if counter_amsatclinic4 < amSat_clinic4:
+                not_met.append("amSat Clinic 4")
+            if counter_amsatclinic1 < amSat_clinic1:
+                not_met.append("amSat Clinic 1")
+            if counter_amsatclinic3 < amSat_clinic3:
+                not_met.append("amSat Clinic 3")
+            if counter_p < p:
+                not_met.append("P")
 
-        # Compare whether the current >= constraints
-        not_met = []
-        if current_total_call < total_call:
-            not_met.append("total_call")
-        if current_clinic1 < clinic1:
-            not_met.append("clinic 1")
-        if current_clinic2 < clinic2:
-            not_met.append("clinic 2")
-        if current_amSat_clinic4 < amSat_clinic4:
-            not_met.append("amSat Clinic 4")
-        if current_amSat_clinic1 < amSat_clinic1:
-            not_met.append("amSat Clinic 1")
-        if current_amSat_clinic3 < amSat_clinic3:
-            not_met.append("amSat Clinic 3")
-        if current_p < p:
-            not_met.append("P")
-
-        # Return True when constraints met, otherwise return the failed constraints
-        if len(not_met) == 0:
-            return True
-        else:
-            return not_met
+        # dictionary to store the overall days and constraints that are not met
+        dict_notmet[day_key] = not_met
 
         # Close connection to DB
         close_connection(conn, cur)
+
+        # Return True when constraints met, otherwise return the failed constraints dictionary in the form: {date:[constraint1,constraint2],date:[constraint1],...}
+        if dict_notmet:
+            return dict_notmet
+        else:
+            return True
     
     except Exception as e:
         return (str(e))
